1c436cf2925f39544e67b4e2ededcb8e401f7bd8
4555duckwok <kyle.duckworth@ge.com>
2016-08-04 13:45:41 -0400

    Update HelloWorld.java

--- HelloWorld/src/main/java/com/kdp/java/HelloWorld.java
+++ HelloWorld/src/main/java/com/kdp/java/HelloWorld.java
@@ -1,16 +1,17 @@
 package com.kdp.java;
 
 public class HelloWorld {
-
+private String test;
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		System.out.println("Hello World!!!");
-		private String test;
+		
 		System.out.println("Revision Test 1");
 		
 	}
-
+private String test2;
 }
